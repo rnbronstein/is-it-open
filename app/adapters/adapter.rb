21724b6522aaf0c394bb7645fead1bed108e7ebf
einarstensson <einar.stensson@gmail.com>
2016-06-05 17:15:48 -0400

    Added URL methods for the google places client

--- app/adapters/adapter.rb
+++ app/adapters/adapter.rb
@@ -1,10 +1,24 @@
 module Adapter
   class GooglePlacesWrapper
+    attr_accessor :key, :url
 
-    def initialize
+    def initialize(location_x: "-33.8670522", location_y: "151.1957362", radius: '100', type: 'restaurant')
+      @location_x = location_x
+      @location_y = location_y
+      @radius = radius
+
       @key = ENV['google_api_key']
-      @url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=' + @key
+
+      @places_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{location_x},#{location_y}&radius=#{radius}&type=#{type}&key=#{@key}"
     end
 
+    def get_search_url(type, keyword)
+      search_keyword = keyword.gsub(" ", "+")
+      "https://maps.googleapis.com/maps/api/place/radarsearch/json?location=#{@location_x},#{@location_y}&radius=#{@radius}&type=#{type}&keyword=#{search_keyword}&key=#{@key}"
+    end
+
+    def get_details_url(place_id)
+      "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place_id}&key=#{@key}"
+    end
   end
 end
