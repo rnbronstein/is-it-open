6c1485ef4610ae752c8d0739f5d086c56b131c84
einarstensson <einar.stensson@gmail.com>
2016-06-06 11:35:00 -0400

    Setup index action to call the default venues API call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,24 @@ class HomeController < ApplicationController
   before_action :initialize_google_client, only: [:index, :search]
 
   def index
+    @venues = []
 
+    # Loop through the default_call and add Venue.new objects to
+    # the @venues array. Each Venue.new object will require its
+    # own call to the details API.
+
+#    @google_client.default_call().each do |venue|
+#      @venues << Venue.new(@google_client.details_call(venue))
+#    end
+
+    #vs.
+    @google_client.default_call()['results'].each do |venue|
+      @venues << @google_client.details_call(venue) #returns a JSON element { name: "Spock's restaurant", }
+    end
   end
 
   def search
     render :json => { response_string: @google_client.search_call()}
-  end
-
-  def default
-    render :json => { response_string: @google_client.default_call()}
   end
 
   private
