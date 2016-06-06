4c91640d82cdd692497b6e37fa496c57bd59cb0c
rnbronstein <rnbronstein@gmail.com>
2016-06-05 23:43:39 -0400

    jquery for showing info functional, but css off

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,1 +1,23 @@
 
+$(function(){
+  $('.venue-info').hide()
+  showInfo()
+  hideInfo()
+
+})
+
+function showInfo(){
+  $('.venue-img').mouseenter(function(e){
+    // alert('made it this far')
+    // debugger
+    $(this).parent().parent().find('.venue-info').slideDown('slow')
+    // debugger
+  })
+}
+
+function hideInfo(){
+  $('.venue-info').mouseleave(function(e){
+    $(this).slideUp('slow')
+    $(this).hide()
+  })
+}
