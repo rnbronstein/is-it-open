f1728ce9b2c0da561703367c2eab48a4d5e058fa
einarstensson <einar.stensson@gmail.com>
2016-06-07 19:00:16 -0400

    Styling, change bg

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,7 +6,7 @@ $(function(){
 
 function showInfo(){
   $('.venue-thumbnail-frame').mouseenter(function(e){
-    $(this).parent().parent().find('.helper img').fadeOut('400')
+    $(this).parent().parent().find('.helper img').hide()
     $(this).parent().parent().find('.venue-info').fadeIn('400')
   })
 }
@@ -15,6 +15,6 @@ function hideInfo(){
   $('.venue-thumbnail-frame').mouseleave(function(e){
     $(this).parent().parent().find('.helper img').fadeIn('400')
     var info = $(this).parent().parent().find('.venue-info')
-    info.fadeOut('400')
+    info.hide()
   })
 }
