e446cd43203408ddf1989490eb7cb40907ef1cba
einarstensson <einar.stensson@gmail.com>
2016-06-07 17:25:43 -0400

    Added animations for the venues

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,13 +6,24 @@ $(function(){
 
 function showInfo(){
   $('.venue-thumbnail-frame').mouseenter(function(e){
+    $(this).parent().parent().find('.helper img').fadeOut('400')
     $(this).parent().parent().find('.venue-info').slideDown('slow')
   })
 }
 
 function hideInfo(){
   $('.venue-thumbnail-frame').mouseleave(function(e){
+    $(this).parent().parent().find('.helper img').fadeIn('400')
     var info = $(this).parent().parent().find('.venue-info')
     info.slideUp('slow')
+  })
+}
+
+function onVenueHover(){
+  $('.helper img').on('click', function(e){
+    e.stopPropagation
+    $(this).fadeOut('400')
+
+    $('venue-info').fadeIn('400')
   })
 }
