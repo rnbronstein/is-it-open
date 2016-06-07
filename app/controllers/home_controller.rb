0fd29a6f05ca2083f1ebd058c9b07a6ffb1db1ba
einarstensson <einar.stensson@gmail.com>
2016-06-07 10:54:04 -0400

    Refactored and removed uneccessary methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-#    @venues = VenueLister.new(set_ip, "restaurant").venues_with_details
-    @venues = VenueFilterer.new(set_ip, "restaurant").filtered_venues
+    @venues = VenueLister.new(set_ip, "restaurant").venues_with_reduced_details
   end
 end
--- app/services/venue_creator.rb
+++ app/services/venue_creator.rb
@@ -1,20 +1,23 @@
 class VenueCreator
-  def initialize(venue)
-    @venue_data = venue
-    @details_client = build_details_client(venue)
+  def initialize(venue_data)
+    @venue_data = venue_data
   end
 
-  def get_venue_details
+  def get_venue_expanded_details
+    @details_client = build_details_client(@venue_data)
+    @venue = Venue.find_by(place_id: @venue_data['place_id'])
+
     venue_details = @details_client.details_call['result']
 
-    if(venue_details['opening_hours'] && venue_details['photos'])
-      Venue.new(formatted_address: venue_details['formatted_address'],
-        formatted_phone_number: venue_details['formatted_phone_number'],
-        name: venue_details['name'],
-        opening_hours: venue_details['opening_hours']['periods'],
-        photo: venue_details['photos'].first['photo_reference']
-      )
+    if venue_details['formatted_phone_number']
+      @venue.update(formatted_phone_number: venue_details['formatted_phone_number'])
     end
+
+    if venue_details['opening_hours']['periods']
+      @venue.update(opening_hours: venue_details['opening_hours']['periods'])
+    end
+
+    binding.pry
   end
 
   def get_reduced_venue_details
@@ -27,7 +30,7 @@ class VenueCreator
 
   private
 
-  def build_details_client(venue)
-    Adapter::PlaceDetailsWrapper.new(venue['place_id'])
+  def build_details_client(venue_data)
+    Adapter::PlaceDetailsWrapper.new(venue_data['place_id'])
   end
 end
