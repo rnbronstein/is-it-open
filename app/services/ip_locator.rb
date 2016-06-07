6ad055b05f4de2ffafb767aeb651023343778afe
rnbronstein <rnbronstein@gmail.com>
2016-06-06 23:45:48 -0400

    working autocomplete address field

--- app/services/ip_locator.rb
+++ app/services/ip_locator.rb
@@ -1,7 +1,7 @@
 class IpLocator
   attr_accessor :latitude, :longitude
 
-  def initialize(ip)
+  def initialize(ip) #this might be a set of coordinates already - that's okay
     coordinates = Geocoder.coordinates(ip)
     @latitude = coordinates.first
     @longitude = coordinates.last
